85de8910ce224aaf5d4ad76045ab7c34ab4d9f44
KhalilLounis <k_lounis@estin.dz>
2024-03-25 03:48:02 +0100

    added slidein animation for about sec

--- src/comps/About.jsx
+++ src/comps/About.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const AboutCard = ({ title, desc }) => {
   return (
     <div className="flex  border-2 border-solid p-4 text-[#d3d3d3] rounded-md border-white flex-col gap-y-2 hover:bg-white hover:text-black transition duration-300 ease-in-out ">
       <h1 className="font-main text-xl font-semibold  ">{title}</h1>
-      <p className=" font-main font-light text-lg  ">
-        {desc}
-      </p>
+      <p className=" font-main font-light text-lg  ">{desc}</p>
       <button className=" w-1/2 self-center bg-[#ffa500] rounded-md text-black font-main p-3 text-md">
         Go
       </button>
@@ -15,6 +14,22 @@ const AboutCard = ({ title, desc }) => {
 };
 
 const About = () => {
+  const slideIn = (delay) => {
+    return {
+      initial: {
+        opacity: 0,
+        x: -500,
+      },
+      animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+          duration: 1,
+          delay: delay,
+        },
+      },
+    };
+  };
   const cards = [
     {
       title: "Web Development",
@@ -36,13 +51,25 @@ const About = () => {
   const spanStyle = "hover:text-[#ffa500] font-bold";
   return (
     <div className="flex flex-col bg-gradient-to-b from-[#0d0d21] to-[#111130] size-screen snap-start ">
-      <div className="flex justify-end">
+      <motion.div
+        whileInView="animate"
+        viewport={{ once: false, amount: 0.25 }}
+        variants={slideIn()}
+        initial="initial"
+        className="flex justify-end"
+      >
         <p className="font-main text-lg text-[#808080] text-center mt-4">
           I focus on helping your brand grow<br></br> and move forward
         </p>
         <hr className="w-1/3 self-center stroke-slate-500 stroke-1 " />
-      </div>
-      <div className="flex flex-col gap-y-4">
+      </motion.div>
+      <motion.div
+        whileInView="animate"
+        viewport={{ once: false, amount: 0.25 }}
+        variants={slideIn()}
+        initial="initial"
+        className="flex flex-col gap-y-4"
+      >
         <h1 className=" text-center font-light text-7xl font-main text-white">
           <span className={spanStyle}>Unique </span>Ideas<br></br>{" "}
           <span className={spanStyle}>For Your </span> Buisness.
@@ -50,12 +77,18 @@ const About = () => {
         <div className=" self-center bg-[#ffa500] rounded-xl text-black font-main p-4 cursor-pointer text-lg">
           WHAT WE DO?
         </div>
-      </div>
-      <div className="flex  gap-x-8 my-8 px-8">
+      </motion.div>
+      <motion.div
+        whileInView="animate"
+        viewport={{ once: false, amount: 0.25 }}
+        variants={slideIn()}
+        initial="initial"
+        className="flex  gap-x-8 my-8 px-8"
+      >
         {cards.map((card, index) => (
           <AboutCard key={index} title={card.title} desc={card.desc} />
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
